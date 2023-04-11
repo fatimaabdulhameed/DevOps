ddc77109b735ed27890412bd4d2e3673529c8120
unknown <fatimaabdulhameed>
2023-04-11 17:10:35 +0500

    first_commit

---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGO_URI : "mongodb+srv://library:CMXOXwBnWufyfX9f@cluster0.zgnefpm.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET : "wg9GbCz3ahZKHs1Pj4iMVQ6FOEqe8Yfr",
+};
---
+++ models/book.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+// mongoose.set('strictQuery', true);
+
+const bookSchema = new mongoose.Schema({
+
+
+    title: {
+        type: String,
+
+    },
+
+    author: {
+        type: String,
+      
+    },
+
+    publisher: {
+        type: String,
+       
+    },
+
+    year: {
+        type: String,
+        default:2021
+    },
+  
+    copies: {
+        type: Number,
+        default:15 
+    }
+
+
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("Book", bookSchema);
